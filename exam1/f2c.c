93abc20b0898bb8ca064cc941942ce91eb51154d
jalubk20 <jalubk20@gmail.com>
2022-02-09 10:10:14 -0500

    exam1

---
+++ exam1/f2c.c
@@ -0,0 +1,9 @@
+
+
+double f2c(double f){
+	double cel = f;	
+	cel = cel-32;
+	cel = cel*5;
+	cel = cel/9;
+	return cel;
+}
---
+++ exam1/f2c.h
@@ -0,0 +1,2 @@
+// takes double, returns double, converts from fahrenheit to celcius
+extern double f2c(double f)
