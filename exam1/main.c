92f019ed6dbc389361bc3276e0d7e2fc90ec93cb
jalubk20 <jalubk20@gmail.com>
2022-02-09 10:11:40 -0500

    exam1

--- exam1/main.c
+++ exam1/main.c
@@ -2,7 +2,7 @@
 #include "f2c.h"
 
 int main(argc, argv){
-//printf("%lf\n", f2c(32)); // freezing point - expect 0
-//printf("%lf\n", f2c(212)); // boiling point - expect 100
-//printf("%lf\n", f2c(20)); // sub-freezing - expect low neg number
+printf("%lf\n", f2c(32)); // freezing point - expect 0
+printf("%lf\n", f2c(212)); // boiling point - expect 100
+printf("%lf\n", f2c(20)); // sub-freezing - expect low neg number
 }
