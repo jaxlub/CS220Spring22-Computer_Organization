afc91b0b6997a25884057911aed16408664ae83d
jalubk20 <jalubk20@gmail.com>
2022-02-16 16:31:32 -0500

    hw2 corrected

--- hw2/int2bin.c
+++ hw2/int2bin.c
@@ -3,18 +3,21 @@
 
 void int2bin(int n){
 	char binary[50];
-	int tracker = 0;	
+	int tracker = 0;
 	while(n > 0){	
-		binary[tracker] = n % 2;
-		tracker++;
-		n = n / 2; 
+		binary[tracker] = n&1;
+		n = n / 2;
+		tracker++; 
+		
+		
 	}
 	binary[tracker] = '\0';
 	char flip[tracker];
-	for(int x = tracker+1; x > 0; x = x - 1){			
-		flip[tracker-x] = binary[x];	
+	for(int x = tracker; x > 0; x = x - 1){			
+		flip[tracker-x] = binary[x-1];	
 	}	
-	for(int i = 0; i < tracker+1; i++){
+
+	for(int i = 0; i < tracker; i++){
 		printf("%d", flip[i]);
 	}
 	printf("\n");
